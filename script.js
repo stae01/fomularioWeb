ec06534befd930308465a9a6c47ee850b385d3b5
stae01 <166432863+stae01@users.noreply.github.com>
2025-03-18 12:26:46 -0700

    script

---
+++ script.js
@@ -0,0 +1,8 @@
+const botonRegistrar = document.getElementById("agregar");
+const tabla = document.getElementById("tablaCarro").getElementsByTagName('tbody')[0];  
+
+
+botonRegistrar.onclick = function(event) {
+    event.preventDefault(); 
+
+}
